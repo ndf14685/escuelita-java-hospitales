bea014ae8c85f2b5f9fe702560c47119d63fb013
Guillermo Molteni <guillem2604@gmail.com>
2018-07-02 20:52:39 -0300

    Dtos de PacienteHospitalMuniz y PacientePeritoMoreno con constructor para el segundo

--- src/escuelita/java/hospital/PacienteHospitalMunizDTO.java
+++ src/escuelita/java/hospital/PacienteHospitalMunizDTO.java
@@ -1,5 +1,33 @@
 package escuelita.java.hospital;
 
 public class PacienteHospitalMunizDTO {
+	String idpaciente;
+	String fechaAlta;
+	String antecedentes[]=new String[3];
+	String notasDelDoctor;
 
+	public String getIdpaciente() {
+		return idpaciente;
+	}
+	public void setIdpaciente(String idpaciente) {
+		this.idpaciente = idpaciente;
+	}
+	public String getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(String fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+	public String[] getAntecedentes() {
+		return antecedentes;
+	}
+	public void setAntecedentes(String[] antecedentes) {
+		this.antecedentes = antecedentes;
+	}
+	public String getNotasDelDoctor() {
+		return notasDelDoctor;
+	}
+	public void setNotasDelDoctor(String notasDelDoctor) {
+		this.notasDelDoctor = notasDelDoctor;
+	}	
 }
