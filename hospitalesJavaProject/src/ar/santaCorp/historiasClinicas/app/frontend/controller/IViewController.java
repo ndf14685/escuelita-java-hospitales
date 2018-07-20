746a588dbb12eccb2ca52339dacfa755174b1ceb
Guillermo Molteni <guillem2604@gmail.com>
2018-07-20 11:47:42 -0300

    borre todo para levantar lo de santamarina

--- hospitalesJavaProject/src/ar/santaCorp/historiasClinicas/app/frontend/controller/IViewController.java
+++
@@ -1,9 +0,0 @@
-package ar.santaCorp.historiasClinicas.app.frontend.controller;
-
-public interface IViewController {
-	public static final boolean AUTH_REQUIRED = false;
-	public void renderView();
-	public void destroy();
-}
-
-
